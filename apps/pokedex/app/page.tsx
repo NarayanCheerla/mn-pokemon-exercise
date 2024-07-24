5b6a516ffa85faf1d491ba2fe17ae08c73d1321b
CNR <narayana.cheerla@gmail.com>
2024-07-24 12:25:42 +0530

    integrated utils package

--- apps/pokedex/app/page.tsx
+++ apps/pokedex/app/page.tsx
@@ -1,5 +1,6 @@
 import { Button } from "@repo/ui/button";
+import {add} from "@repo/utils";
 
 export default function Home() {
-  return <Button appName="Pokedex">Open alert pokedex</Button>;
+  return <Button appName="Pokedex">Open alert pokedex{add(5,6)}</Button>;
 }
